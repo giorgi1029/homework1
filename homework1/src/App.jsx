1f78b328b22674a5bd587c807de5c31d73434431
giorgi khizanishvili <gkhizanishvili18@gmail.com>
2024-09-26 23:14:14 +0400

    first commit

---
+++ homework1/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='main'>
+    <h2>სახელი: გიორგი</h2>
+    <h2>გვარი: ხიზანიშვილი</h2>
+    <h2>ასაკი: 15</h2>
+    <h2>ჰობი: კალათბურთი და პროგრამირება</h2>
+    </div>
+  )
+}
+
+export default App
